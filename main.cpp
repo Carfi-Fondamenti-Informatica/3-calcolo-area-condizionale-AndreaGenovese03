8bd70f1bbcd3f70c66c0ee9085035f083a5c0695
AndreaGenovese03 <118006599+AndreaGenovese03@users.noreply.github.com>
2022-11-18 21:37:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,32 @@
 using namespace std;
 
 int main() {
+   float a=0;
+    float b=0;
+    int opzione;
+
+    cout << "Inserisci il valore di a: " << endl;
+    cin >> a;
+    cout << "Inserisci il valore di b: " << endl;
+    cin >> b;
+    cout << "Inserisci opzione (0 Triangolo, 1 Quadrato, 2 Rettangolo): " << endl;
+    cin >> opzione;
+
+        switch (opzione){
+            case 0:
+                cout << (a*b)/2 << endl;
+                break;
+
+            case 1:
+                cout << (a*a) << endl;
+                break;
+
+            case 2:
+                cout << (a*b) << endl;
+                break;
+
+            default:
+                cout << "Opzione non valida." << endl;
+        }
    return 0;
 }
